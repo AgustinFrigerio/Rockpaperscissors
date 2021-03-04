aa5b399ec12c221261fef833976a2a1b8f8f1b64
agustin <agustinfrigerio10@gmail.com>
2021-03-04 18:40:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+function newRound (playerSelection){
+
+
+const options = ['rock' 'paper' 'scissors']
+function computerPlay(){
+   const optionsPick = Math.floor(Math.random() * options.length);
+    return options [optionsPick];
+}
+let computerSelection = computerPlay();
+function playRound(playerSelection, computerSelection){
+    if (computerSelection==playerSelection)
+    tie;
+    else if (playerSelection === 'rock' && computerSelection === 'paper')
+    lose;
+    else if (playerSelection === 'rock' && computerSelection === 'scissors')
+    won;
+    else if (playerSelection === 'scissors' && computerSelection === 'rock')
+    lose;  
+    else if (playerSelection === 'scissors' && computerSelection === 'paper')
+    won;
+    else if (playerSelection === 'paper' && computerSelection === 'scissors')
+    lose;
+    else if (playerSelection === 'paper' && computerSelection === 'rock')
+    won;
+}
+}
+function tie (){
+    const text = document.getElementById("resultext");
+    text.classList.add('resultext');
+    text.textContent ='Es un empate perro';
+}
+function won (){
+    const text = document.getElementById("resultext");
+    text.classList.add('resultext');
+    text.textContent ='Ganaste vieja!!!!';
+}
+function lose (){
+    const text = document.getElementById("resultext");
+    text.classList.add('resultext');
+    text.textContent ='F, perdiste contra una computadora';
+}
