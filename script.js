1583a8230794a2eb23a17f2a39674e6e44788633
agustin <agustinfrigerio10@gmail.com>
2021-03-08 13:02:48 -0300

    nueva version

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-
-function newRound (playerSelection){
-
-
-const options = ['rock' 'paper' 'scissors']
-function computerPlay(){
-   const optionsPick = Math.floor(Math.random() * options.length);
-    return options [optionsPick];
-}
 let computerSelection = computerPlay();
-function playRound(playerSelection, computerSelection){
-    if (computerSelection==playerSelection)
-    tie;
-    else if (playerSelection === 'rock' && computerSelection === 'paper')
-    lose;
-    else if (playerSelection === 'rock' && computerSelection === 'scissors')
-    won;
-    else if (playerSelection === 'scissors' && computerSelection === 'rock')
-    lose;  
-    else if (playerSelection === 'scissors' && computerSelection === 'paper')
-    won;
-    else if (playerSelection === 'paper' && computerSelection === 'scissors')
-    lose;
-    else if (playerSelection === 'paper' && computerSelection === 'rock')
-    won;
+let playerSelection = prompt("Choose rock, paper or scissors");
+
+console.log(playRound(playerSelection, computerSelection));
+function playRound(param1, param2){
+  const options = ["rock", "paper", "scissors"];
+  let won = "Ganaste vieja!";
+  let lose = "F, perdiste contra una computadora";
+  let tie = "Es un empate perro";
+    if (param1===param2){
+      return tie;
+    } else if (param1 === "rock" && param2 === "paper"){
+      return lose;
+    } else if (param1 === "rock" && param2 === "scissors"){
+      return won;
+    } else if (param1 === "scissors" && param2 === "rock"){
+      return lose;
+    } else if (param1 === "scissors" && param2 === "paper"){
+      return won;
+    } else if (param1 === "paper" && param2 === "scissors"){
+      return lose;
+    } else if (param1 === "paper" && param2 === "rock"){
+      return won;
+    }    
 }
-}
-function tie (){
-    const text = document.getElementById("resultext");
-    text.classList.add('resultext');
-    text.textContent ='Es un empate perro';
-}
-function won (){
-    const text = document.getElementById("resultext");
-    text.classList.add('resultext');
-    text.textContent ='Ganaste vieja!!!!';
-}
-function lose (){
-    const text = document.getElementById("resultext");
-    text.classList.add('resultext');
-    text.textContent ='F, perdiste contra una computadora';
-}
-asda
+function computerPlay(){
+  const options = ["rock", "paper", "scissors"];
+  const optionsPick = Math.floor(Math.random()*options.length);
+   return options[optionsPick];}
+const options = ["rock", "paper", "scissors"];
+//function tie (){
+   // const text = document.getElementById("resultext");
+   /// text.classList.add('resultext');
+   // text.textContent ='Es un empate perro';
+//function won (){
+  // const text = document.getElementById("resultext");
+    //text.classList.add('resultext');
+   // text.textContent ='Ganaste vieja!!!!';
+//function lose (){
+  //  const text = document.getElementById("resultext");
+  //  text.classList.add('resultext');
+  //  text.textContent ='F, perdiste contra una computadora';
